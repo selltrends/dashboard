ef9c0d45ede2198fdf596cfc15e345695719de3f
Alex Wang <selltrends@gmail.com>
2026-06-26 08:04:08 +0800

    wagtail start dashboard .

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Wagtail + Tailwindcss 4 + Daisyui + Vite Dashboard
 
+
+## Development Setup
+
+1. Prerequiisties
+
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install wagtail
+wagtail start dashboard .
---
+++ dashboard/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-qibenm568606v#w*-fuqa4x$_)9a31qp@6)o4mnc=cgz*+26pv"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
